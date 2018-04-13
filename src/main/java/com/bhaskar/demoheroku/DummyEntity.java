48058723930e7e6a23207627f18429b886fecd79
Bhaskar, Anil <anil.bhaskar@rakuten.com>
2018-04-13 15:39:37 +0530

    adding an entity

---
+++ src/main/java/com/bhaskar/demoheroku/DummyEntity.java
@@ -0,0 +1,22 @@
+package com.bhaskar.demoheroku;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dummy")
+public class DummyEntity {
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "dummy")
+  private String dummy;
+
+}
